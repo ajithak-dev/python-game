6beab9debdf2833a09fdc98b9187caf54066fe32
azuzi01234@gmail.com <azuzi01234@gmail.com>
2023-09-13 14:26:35 +0530

    file added

--- game_rps.py
+++ game_rps.py
@@ -1,12 +1,11 @@
 import random
 option=[1,2,3]
 randomoption=random.choice(option)
-counter=0
-#getting input
+
 
 for i in range(1,11):
     #getting input
-    userinput=(input("enter the option r for 'Rock', p for 'Paper' and s for 'Sicssor'"))
+    userinput=(input("enter the option r for 'Rock', p for 'Paper' and s for 'Scissor'"))
     if userinput == 'r' or userinput =='R':
         userinput=1
     elif userinput == 'p' or userinput =='P':
@@ -14,59 +13,48 @@ for i in range(1,11):
     else:
         userinput=3
 
-    print(userinput)
-    print(randomoption)
     if userinput==1:
         if userinput == randomoption:
-            print("it's a DRAW you choose 'Rock' and Computer Choose 'Rock', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("it's a DRAW \nyou choose 'Rock' and Computer Choose 'Rock', The remaining Chances are = ",i if i == 0 else 10 - i)
+           
             
         elif userinput<randomoption:
-            print("you LOST you choose 'Rock' and Computer Choose 'Paper', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("you LOST \nyou choose 'Rock' and Computer Choose 'Paper', The remaining Chances are = ",i if i == 0 else 10 - i)
+         
             
         elif userinput>randomoption:
-            print("you WON you choose 'Rock' and Computer Choose 'Sicssor', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("you WON \nyou choose 'Rock' and Computer Choose 'Scissor', The remaining Chances are = ",i if i == 0 else 10 - i)
+            
             
         
     elif userinput == 2:
         if userinput == randomoption:
-            print("it's a DRAW you choose 'Paper' and Computer Choose 'Paper', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("it's a DRAW \nyou choose 'Paper' and Computer Choose 'Paper', The remaining Chances are = ",i if i == 0 else 10 - i)
+           
             
         elif userinput<randomoption:
-            print("you LOST you choose 'Paper' and Computer Choose 'Sicssor', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("you LOST \nyou choose 'Paper' and Computer Choose 'Scissor', The remaining Chances are = ",i if i == 0 else 10 - i)
+            
             
         elif userinput>randomoption:
-            print("you WON you choose 'Paper' and Computer Choose 'Rock', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("you WON \nyou choose 'Paper' and Computer Choose 'Rock', The remaining Chances are = ",i if i == 0 else 10 - i)
+            
             
         
     else:
         if userinput == randomoption:
-            print("it's a DRAW you choose 'Sicssor' and Computer Choose 'Sicssor', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("it's a DRAW \nyou choose 'Scissor' and Computer Choose 'Scissor', The remaining Chances are = ",i if i == 0 else 10 - i)
+            
             
         elif userinput==3 and randomoption==2:
-            print("you WON you choose 'Sicssor' and Computer Choose 'Sicssor', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("you WON \nyou choose 'Scissor' and Computer Choose 'Paper', The remaining Chances are = ",i if i == 0 else 10 - i)
+            
             
         elif userinput>randomoption:
-            print("you LOST you choose 'Sicssor' and Computer Choose 'Rock', The remaining Chances are = ",i if i == 0 else 10 - i)
-            counter=counter+1
+            print("you LOST \nyou choose 'Sicssor' and Computer Choose 'Rock', The remaining Chances are = ",i if i == 0 else 10 - i)
             
-        
-    
-print("You Won = ",counter)
-computerwins=counter-10
-print("Computer Won = ",computerwins)
+            
 
-if counter > computerwins:
-    print("THEN.....\n You're the Overall  Winner")
-else:
-    print("THEN.....\n Computer is the Overall Winner")
 
 
 
